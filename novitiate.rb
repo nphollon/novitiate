a69d37c1d11f7dbbabde7fb3fc914dee7d1307c6
nphollon <nhollon@thoughtworks.com>
2013-03-02 13:11:36 -0600

    Add modulation settings (don't yet affect audio output)

--- novitiate.rb
+++ novitiate.rb
@@ -1,84 +1,117 @@
-require 'waveform'
-require 'portaudio'
+require_relative './waveform'
+require_relative '../ruby-portaudio/lib/portaudio'
 
 class Novitiate
   include Waveform
 
-  attr_reader :wave_setting, :gain, :waveform, :frequency_setting, :buffer
+  attr_reader :buffer, :step,
+    :osc_wave_setting, :osc_waveform, :osc_freq_setting, 
+    :mod_wave_setting, :mod_waveform, :mod_freq_setting
+  attr_accessor :gain
 
   def initialize
-    @wave_setting = :sine
     @gain = 1.0
-    @phase = 0
-    @waveform = SineWave.new(440)
-    self.frequency_setting = 0.5
+    @phase = 0.0
+
+    @osc_wave_setting = :sine
+    @osc_waveform = SineWave.new(440.0)
+    self.osc_freq_setting = 0.5
+
+    @mod_wave_setting = :sine
+    @mod_waveform = SineWave.new(440.0)
+    self.mod_freq_setting = 0.0
   end
 
   def turn_on
     PortAudio.init
-    device = PortAudio.default_output_device
-    @stream = device.open_stream(format: :float32)
+    @device = PortAudio.default_output_device
     @buffer = PortAudio::SampleBuffer.new(format: :float32)
-    @step = device.default_sample_rate ** -1
+    @step = @device.default_sample_rate ** -1
   end
 
   def turn_off
     @buffer.dispose
-    @stream.close
     PortAudio.terminate
   end
 
   def play
-    @stream.start
+    stream = @device.open_stream(format: :float32)
+    stream.start
     time = 0
 
     loop do
       yield time
-      @stream << fill_buffer
+      stream << fill_buffer
       time += @step * @buffer.frames
     end
     
-    @stream.stop
+    stream.stop
+    stream.close
   end
 
   def fill_buffer
     @buffer.fill do
-      @phase = (@phase + self.frequency*@step).modulo(1)
-      sample @waveform.eval(@phase)
+      @phase = (@phase + self.osc_frequency*@step).modulo(1)
+      sample @osc_waveform.eval(@phase)
     end
   end
 
   def sample normal_value
-    2 * normal_value - 1
+    @gain * (2 * normal_value - 1)
   end
 
-  def wave_setting=(new_setting)
-    @wave_setting = new_setting
-    @waveform = {
+  def osc_wave_setting=(new_setting)
+    @osc_wave_setting = new_setting
+    @osc_waveform = {
       sine: SineWave,
       triangle: TriangleWave,
       square: SquareWave,
       sawtooth: SawtoothWave
-      }[new_setting].new( frequency )
+      }[new_setting].new( osc_frequency )
   end
 
-  def frequency_setting=(new_setting)
-    raise RangeError, "Frequency setting must be between 0 and 1." unless (0..1).include?(new_setting)
-    @frequency_setting = new_setting
-    @waveform.frequency = 2 * 10 ** (3*new_setting + 1)
-  end
-
-  def frequency=(new_frequency)
-    self.frequency_setting = (Math.log10(0.5*new_frequency) - 1) / 3
+  def osc_freq_setting=(new_setting)
+    raise RangeError, "oscillation frequency setting must be between 0 and 1." unless (0..1).include?(new_setting)
+    @osc_freq_setting = new_setting
+    @osc_waveform.frequency = 2 * 10 ** (3*new_setting + 1)
   end
 
   def slew_frequency(slew_rate) # in octaves per second
-    self.frequency_setting += slew_rate * Math.log10(2)/3 * @step * @buffer.frames
+    self.osc_freq_setting += slew_rate * Math.log10(2)/3 * @step * @buffer.frames
   end
 
-  def frequency
-    @waveform.frequency
+  def osc_frequency=(new_frequency)
+    self.osc_freq_setting = (Math.log10(0.5*new_frequency) - 1) / 3
   end
+
+  def osc_frequency
+    @osc_waveform.frequency
+  end
+
+  def mod_wave_setting=(new_setting)
+    @mod_wave_setting = new_setting
+    @mod_waveform = {
+      sine: SineWave,
+      triangle: TriangleWave,
+      square: SquareWave,
+      sawtooth: SawtoothWave
+      }[new_setting].new( mod_frequency )
+  end
+
+  def mod_freq_setting=(new_setting)
+    raise RangeError, "modulation frequency setting must be between 0 and 1." unless (0..1).include?(new_setting)
+    @mod_freq_setting = new_setting
+    @mod_waveform.frequency = 2 * 10 ** (3*new_setting + 1)
+  end
+
+  def mod_frequency=(new_frequency)
+    self.mod_freq_setting = (Math.log10(0.5*new_frequency) - 1) / 3
+  end
+
+  def mod_frequency
+    @mod_waveform.frequency
+  end
+
 
   def playing?
     false
--- spec/novitiate_spec.rb
+++ spec/novitiate_spec.rb
@@ -13,78 +13,160 @@ describe "Novitiate" do
 
   subject { @nov }
 
-  describe "oscillation" do
-    its (:wave_setting) { should == :sine }
-    its (:frequency_setting) { should == 0.5 }
+  describe "defaults" do
+    its (:osc_wave_setting) { should == :sine }
+    its (:osc_freq_setting) { should == 0.5 }
+    its (:mod_wave_setting) { should == :sine }
+    its (:mod_freq_setting) { should == 0.0 }
     its (:gain) { should == 1.0 }
+    its (:step) { should == 44100.0**-1 }
+  end
 
+  describe "oscillation" do
     describe "wave_settings" do
       it "gives sine wave output by default" do
-        @nov.waveform.class.should == Novitiate::SineWave
+        @nov.osc_waveform.class.should == Novitiate::SineWave
       end
 
       specify "triangle wave setting" do
-        @nov.wave_setting = :triangle
-        @nov.waveform.class.should == Novitiate::TriangleWave
+        @nov.osc_wave_setting = :triangle
+        @nov.osc_waveform.class.should == Novitiate::TriangleWave
       end
 
       specify "square wave setting" do
-        @nov.wave_setting = :square
-        @nov.waveform.class.should == Novitiate::SquareWave
+        @nov.osc_wave_setting = :square
+        @nov.osc_waveform.class.should == Novitiate::SquareWave
       end
 
       specify "sawtooth wave setting" do
-        @nov.wave_setting = :sawtooth
-        @nov.waveform.class.should == Novitiate::SawtoothWave
+        @nov.osc_wave_setting = :sawtooth
+        @nov.osc_waveform.class.should == Novitiate::SawtoothWave
       end
     end
 
     describe "frequency settings" do
       before do
-        @freq1 = @nov.frequency
-        @nov.frequency_setting = 0.6
+        @freq1 = @nov.osc_frequency
+        @nov.osc_freq_setting = 0.6
       end
 
-      its(:frequency) { should be > @freq1 }
-      its(:frequency) { should == @nov.waveform.frequency }
+      its(:osc_frequency) { should be > @freq1 }
+      its(:osc_frequency) { should == @nov.osc_waveform.frequency }
 
       specify "logarithmically-scaled frequency" do
-        freq2 = @nov.frequency
-        @nov.frequency_setting = 0.7
-        (@nov.frequency / freq2).should be_within(1e-3).of(freq2 / @freq1)
+        freq2 = @nov.osc_frequency
+        @nov.osc_freq_setting = 0.7
+        (@nov.osc_frequency / freq2).should be_within(1e-3).of(freq2 / @freq1)
       end
 
       it "can't have a frequency setting higher than 1" do
-        expect { @nov.frequency_setting = 1.001 }.to raise_error(RangeError)
+        expect { @nov.osc_freq_setting = 1.001 }.to raise_error(RangeError)
       end
 
       it "can't have a frequency setting lower than 0" do
-        expect { @nov.frequency_setting = -0.001 }.to raise_error(RangeError)
+        expect { @nov.osc_freq_setting = -0.001 }.to raise_error(RangeError)
       end
 
       it "can turn frequency knob in units of octaves per second" do
-        freq2 = @nov.frequency
+        freq2 = @nov.osc_frequency
         @nov.slew_frequency(1)
-        @nov.frequency.should be_within(1e-6).of(1.0162250658168974*freq2)
+        @nov.osc_frequency.should be_within(1e-6).of(1.0162250658168974*freq2)
       end
 
       it "can set frequency directly" do
-        @nov.frequency = @freq1
-        @nov.frequency_setting.should == 0.5
+        @nov.osc_frequency = @freq1
+        @nov.osc_freq_setting.should == 0.5
+      end
+    end
+  end
+
+  describe "modulation" do
+    describe "wave_settings" do
+      it "gives sine wave output by default" do
+        @nov.mod_waveform.class.should == Novitiate::SineWave
+      end
+
+      specify "triangle wave setting" do
+        @nov.mod_wave_setting = :triangle
+        @nov.mod_waveform.class.should == Novitiate::TriangleWave
+      end
+
+      specify "square wave setting" do
+        @nov.mod_wave_setting = :square
+        @nov.mod_waveform.class.should == Novitiate::SquareWave
+      end
+
+      specify "sawtooth wave setting" do
+        @nov.mod_wave_setting = :sawtooth
+        @nov.mod_waveform.class.should == Novitiate::SawtoothWave
       end
     end
 
-    describe "playing" do
-      describe "buffer continuity" do
-        it "should keep track of where it left off when filling multiple buffers" do
-          @nov.wave_setting = :sawtooth
-          @nov.fill_buffer
-          sample1 = @nov.buffer[0,0]
-          @nov.fill_buffer
-          @nov.buffer[0,0].should be > sample1
+    describe "frequency settings" do
+      before do
+        @freq1 = @nov.mod_frequency
+        @nov.mod_freq_setting = 0.1
+      end
+
+      its(:mod_frequency) { should be > @freq1 }
+      its(:mod_frequency) { should == @nov.mod_waveform.frequency }
+
+      specify "logarithmically-scaled frequency" do
+        freq2 = @nov.mod_frequency
+        @nov.mod_freq_setting = 0.2
+        (@nov.mod_frequency / freq2).should be_within(1e-3).of(freq2 / @freq1)
+      end
+
+      it "can't have a frequency setting higher than 1" do
+        expect { @nov.mod_freq_setting = 1.001 }.to raise_error(RangeError)
+      end
+
+      it "can't have a frequency setting lower than 0" do
+        expect { @nov.mod_freq_setting = -0.001 }.to raise_error(RangeError)
+      end
+
+      it "can set frequency directly" do
+        @nov.mod_frequency = @freq1
+        @nov.mod_freq_setting.should == 0.0
+      end
+    end
+  end
+
+  describe "gain settings" do
+    before do
+      @nov.osc_wave_setting = :square
+      @nov.osc_freq_setting = 1.0
+    end
+
+    describe "full gain" do
+      it "should have samples in (-1..1)" do
+        @nov.fill_buffer
+        (0...@nov.buffer.frames).each do |i|
+          @nov.buffer[i,0].abs.should be_within(1e-3).of(1)
         end
       end
     end
 
+    describe "half gain" do
+      it "should have samples in (-0.5..0.5)" do
+        @nov.gain = 0.5
+        @nov.fill_buffer
+        (0...@nov.buffer.frames).each do |i|
+          @nov.buffer[i,0].abs.should be_within(1e-3).of(0.5)
+        end
+      end
+    end
+  end
+
+  describe "playing" do
+    describe "buffer continuity" do
+      it "should keep track of where it left off when filling multiple buffers" do
+        @nov.osc_wave_setting = :sawtooth
+        @nov.fill_buffer
+        sample1 = @nov.buffer[@nov.buffer.frames-1,0]
+        @nov.fill_buffer
+        @nov.buffer[0,0].should be_within(1e-6).of(sample1 + @nov.osc_frequency/22050.0)
+      end
+    end
   end
 end
